8d741e3e5c971220ff7b5bdd0091df3fa7f19ccd
xis <xis@schowek.net>
2018-07-13 09:38:17 +0200

    Unised MongoClient removed

--- src/main/java/net/schowek/xis/example/storage/MongoClient.java
+++
@@ -1,6 +0,0 @@
-package net.schowek.xis.example.storage;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface MongoClient extends MongoRepository<InvocationDocument, String> {
-}
