ed9c9d91f0eeef2b167fccc55952ade8c0eae305
xis <xis@schowek.net>
2018-12-14 12:55:59 +0100

    Example controller cleanup

--- src/main/java/net/schowek/xis/example/HelloController.java
+++ src/main/java/net/schowek/xis/example/HelloController.java
@@ -1,6 +1,5 @@
 package net.schowek.xis.example;
 
-import net.schowek.xis.spring.postpones.PostponedOperationsInvoker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,21 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     private final HelloService helloService;
-    private final PostponedOperationsInvoker postponedOperationsInvoker;
 
     @Autowired
-    public HelloController(HelloService helloService, PostponedOperationsInvoker postponedOperationsInvoker) {
+    public HelloController(HelloService helloService) {
         this.helloService = helloService;
-        this.postponedOperationsInvoker = postponedOperationsInvoker;
     }
 
     @GetMapping("/hello/{name}")
     public void hello(@PathVariable("name") String name) {
         helloService.hello(new Greeting(name));
-    }
-
-    @GetMapping("/run")
-    public void run() {
-        postponedOperationsInvoker.invokeQueued();
     }
 }
